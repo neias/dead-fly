9ccc3e4557056b82a2788103c11cfaa715e7f6f1
Emre Acar <emreacar@Emre-iMac.local>
2018-11-20 21:13:02 +0300

    start

---
+++ Main.py
@@ -0,0 +1,32 @@
+import pygame, sys
+from classes import *
+from process import process
+
+
+pygame.init()
+SCREENWIDTH, SCREENHEIGHT = 640, 360
+screen = pygame.display.set_mode((SCREENWIDTH, SCREENHEIGHT), 0, 32)
+clock = pygame.time.Clock()
+FPS = 24
+total_frames = 0
+
+background = pygame.image.load("images/forest.jpg")
+bug = Bug(0, SCREENHEIGHT - 40, 40, 40, "images/bug.png")
+
+
+while True:
+    process(bug, FPS, total_frames)
+    #LOGIC
+    bug.motion(SCREENWIDTH, SCREENHEIGHT)
+    Fly.movement(SCREENWIDTH)
+    BugProjectile.movement()
+    total_frames += 1
+    #LOGIC
+    #DRAW
+    screen.blit(background, (0,0))
+    BaseClass.allsprites.draw(screen)
+    BugProjectile.List.draw(screen)
+    pygame.display.flip()
+    #DRAW
+
+    clock.tick(FPS)
---
+++ process.py
@@ -0,0 +1,52 @@
+import pygame, sys, classes, random
+
+def process(bug, FPS, total_frames):
+    #PROCESSES
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit
+            sys.exit()
+    keys = pygame.key.get_pressed()
+
+    if keys[pygame.K_d]:
+        classes.Bug.going_right = True
+        bug.image = pygame.image.load("images/bug.png")
+        bug.velx = 5
+    elif keys[pygame.K_a]:
+        classes.Bug.going_right = False
+        bug.image = pygame.image.load("images/bugflipped.png")
+        bug.velx = -5
+    else:
+        bug.velx = 0
+
+    if keys[pygame.K_w]:
+        bug.jumping = True
+
+    if keys[pygame.K_SPACE]:
+        p = classes.BugProjectile(bug.rect.x, bug.rect.y, 43, 25, "images/projectiles/fire.png")
+        if classes.Bug.going_right:
+            p.velx = 8
+        else: 
+            p.image = pygame.transform.flip(p.image, True, False)
+            p.velx = -8
+
+
+    spawn(FPS, total_frames)
+    #PROCESSES
+
+
+def spawn(FPS, total_frames):
+
+    four_seconds = FPS * 4
+
+    if total_frames % four_seconds == 0:
+        r = random.randint(1, 2)
+        x = 1
+        if r == 2:
+            x = 640 - 40 
+
+        classes.Fly(x, 130, 40, 35, "images/fly.png")
+
+
+
+
